ff2d5ac758b1ecf6e0a94029326dbe73ef28a0f3
Luciana Rocha <lucianaferreiradarocha@gmail.com>
2019-11-21 21:27:10 -0300

    feat: Create ddpg_agent with actor and critic net

---
+++ ddpg_agent.py
@@ -0,0 +1,59 @@
+from collections import namedtuple, deque
+
+import numpy as np
+import random
+import torch
+import torch.nn.functional as F
+import torch.optim as optim
+
+from model import Actor, Critic
+
+BUFFER_SIZE = int(1e5)  # replay buffer size
+BATCH_SIZE = 64         # minibatch size
+GAMMA = 0.99            # discount factor
+TAU = 1e-2              # for soft update of target parameters
+LR_actor = 5e-4         # learning rate for actor
+LR_critic = 5e-4        # learning rate for critic
+UPDATE_EVERY = 4        # how often to update the network
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+class Agent():
+    """Interacts with and learns from the environment."""
+
+    def __init__(self, state_size, action_size, seed):
+        """Initialize an Agent object.
+
+        Params
+        ======
+            state_size (int): dimension of each state
+            action_size (int): dimension of each action
+            seed (int): random seed
+        """
+        self.state_size = state_size
+        self.action_size = action_size
+        self.seed = random.seed(seed)
+
+        # Actor-Network
+        self.actor_local = Actor(state_size, action_size, seed).to(device)
+        self.actor_target = Actor(state_size, action_size, seed).to(device)
+        self.actor_optimizer = optim.Adam(self.actor_local.parameters(), lr=LR_actor)
+
+        # Critic-Network
+        self.critic_local = Critic(state_size, action_size, seed).to(device)
+        self.critic_target = Critic(state_size, action_size, seed).to(device)
+        self.critic_optimizer = optim.Adam(self.critic_local.parameters(), lr=LR_critic)
+
+        # Replay memory
+        self.memory = ReplayBuffer(action_size, BUFFER_SIZE, BATCH_SIZE, seed)
+        # Initialize time step (for updating every UPDATE_EVERY steps)
+        self.t_step = 0
+
+    def step():
+        pass
+
+    def act():
+        pass
+
+    def learn():
+        pass
